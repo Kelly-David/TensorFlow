17019aa577963a9bc7f6d0fb8e0b10a25663ed45
kelly-david <davidkelly.tlf@gmail.com>
2018-01-27 13:49:53 +0000

    House Price Prediction

--- tensorflow/house_price_prediction.py
+++ tensorflow/house_price_prediction.py
@@ -7,6 +7,21 @@
 import tensorflow as tf 
 import numpy as np 
 import math 
-import mathplotlib.pyplot as plt
-import mathplotlib.animation as animation
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
+# Generate houses
+num_house = 160
+np.random.seed(42)
+house_size = np.random.randint(low = 1000, high = 3500, size = num_house )
+
+
+# Generate prices
+np.random.seed(42)
+house_price = house_size * 100.0 + np.random.randint(low = 20000, high = 70000, size = num_house )
+
+# Plot the generated house and size
+plt.plot(house_size, house_price, 'bx') # bx = blue
+plt.xlabel('Price')
+plt.ylabel('Size')
+plt.show()
