4f0510237d874770fae93a10f7851b527e0c5cc8
kelly-david <davidkelly.tlf@gmail.com>
2018-01-27 13:10:31 +0000

    Add libs

--- tensorflow/house_price_prediction.py
+++ tensorflow/house_price_prediction.py
@@ -3,3 +3,10 @@
 # A very simple model to predict house price based on size. 
 # Implemented in TensorFlow
 #
+
+import tensorflow as tf 
+import numpy as np 
+import math 
+import mathplotlib.pyplot as plt
+import mathplotlib.animation as animation
+
